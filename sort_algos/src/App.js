136099f71896b945ea37de251c29624e0ab5a338
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-19 23:02:02 +0200

    create class app

--- sort_algos/src/App.js
+++ sort_algos/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
 
+     }
+  }
+  render() { 
+    return (
+       
+
+     );
+  }
+}
+ 
 export default App;
