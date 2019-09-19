3503c4c6971416d34c6f66833a0306002f836505
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-19 23:09:49 +0200

    code indentation

--- sort_algos/src/App.js
+++ sort_algos/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import styled from "styled-components";
 
 const StyledApp = styled.div`
-display: flex;
+  display: flex;
 `;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      bubble: [2,5,8,3,1,4,6,9,7,10],
-     }
+    this.state = {
+      bubble: [2, 5, 8, 3, 1, 4, 6, 9, 7, 10]
+    };
   }
-  render() { 
-    return ( <StyledApp>
-    {this.state.bubble.map(num => <div style={{width: "1rem", height: `${num}rem`, backgroundColor: "black"}}/>)}
-    </StyledApp>
-     );
+  render() {
+    return (
+      <StyledApp>
+        {this.state.bubble.map(num => (
+          <div
+            style={{
+              width: "1rem",
+              height: `${num}rem`,
+              backgroundColor: "black"
+            }}
+          />
+        ))}
+      </StyledApp>
+    );
   }
 }
- 
+
 export default App;
