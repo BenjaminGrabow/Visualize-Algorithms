c0fb90754fd37292b1ebd43bf12ef6be9cb82347
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-20 13:08:30 +0200

    delete react-router and install react-router-dom

--- sort_algos/src/index.js
+++ sort_algos/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Router as BrowserRouter } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
