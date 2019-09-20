b5bb8de75087e09f6a0c2fcec4f5930e5da6924b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-20 13:03:55 +0200

    import router

--- sort_algos/src/index.js
+++ sort_algos/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Router as BrowserRouter } from "react-router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
