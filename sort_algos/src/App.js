c03144f857c3d004415078aee5e60bcf596dda28
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-20 11:51:56 +0200

    new component state and if else condition with stringify

--- sort_algos/src/App.js
+++ sort_algos/src/App.js
@@ -24,44 +24,52 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bubble: [
-        41,
-      ]
+      bubble: [1],
+      bubbleSolved: null
     };
   }
 
   componentDidMount = () => {
     // create array with random numbers (but unique number)
-    
-    for (var a=[],i=0;i<40;++i) a[i]=i;
+
+    for (var a = [], i = 0; i < 40; ++i) a[i] = i;
 
     function shuffle(array) {
-      var tmp, current, top = array.length;
-      if(top) while(--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-      }
+      var tmp,
+        current,
+        top = array.length;
+      if (top)
+        while (--top) {
+          current = Math.floor(Math.random() * (top + 1));
+          tmp = array[current];
+          array[current] = array[top];
+          array[top] = tmp;
+        }
       return array;
     }
-    
+
     a = shuffle(a);
+    let copy = [...a];
 
     this.setState({
       bubble: a
     });
+
+    setTimeout(
+      () =>
+        this.setState(prevState => ({
+          bubbleSolved: copy.sort((a, b) => a - b)
+        })),
+      1000
+    );
   };
 
   sort = () => {
     let arr = [...this.state.bubble];
-    
-//     const arrSorted = this.state.bubble.sort((a, b) => a - b);
-// console.log(arr)
-//     if (arrSorted === arr) {
-//       return null;
 
-//     } else {
+    if (JSON.stringify(this.state.bubbleSolved) === JSON.stringify(arr)) {
+      return null;
+    } else {
       if (arr[i] > arr[i + 1]) {
         setTimeout(() => {
           var temp = arr[i];
@@ -78,7 +86,7 @@ class App extends React.Component {
             i = 0;
             this.sort();
           }
-        }, 100);
+        }, 50);
       } else {
         if (i < arr.length) {
           i += 1;
@@ -88,7 +96,7 @@ class App extends React.Component {
           this.sort();
         }
       }
-    // }
+    }
   };
 
   render() {
