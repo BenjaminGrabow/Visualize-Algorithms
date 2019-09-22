a4de5f90a64485ceb80d590ffbed6f07f941e6f2
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-22 20:27:53 +0200

    added styling to SelectionSort

--- sort_algos/src/components/SortAlgos/SelectionSort.js
+++ sort_algos/src/components/SortAlgos/SelectionSort.js
@@ -2,21 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledSelectionSort = styled.div`
+  .selection {
+    display: flex;
+  }
+
+  .beam {
+    margin: 1rem;
+    background-color: black;
+    width: 1rem;
+  }
 `;
 
 class SelectionSort extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       selection: [1],
       selectionSolved: null
-     }
+    };
   }
 
   componentDidMount = () => {
     // create array with random numbers (but unique number)
 
-    for (var a = [], i = 1; i < 40; ++i) a[i] = i;
+    for (var a = [], i = 0; i < 40; ++i) a[i] = i;
 
     function shuffle(array) {
       var tmp,
@@ -41,22 +50,26 @@ class SelectionSort extends React.Component {
     });
   };
 
-  sort = () => {
-
-  };
+  sort = () => {};
 
   restart = () => {
-  this.componentDidMount();
+    this.componentDidMount();
   };
 
-  render() { 
-    return ( <StyledSelectionSort>
-      {this.state.selection.map(item => <div className="beam" style={{height: `${item}rem`}}/>)}
+  render() {
+    return (
+      <StyledSelectionSort>
+        <div className="selection">
+          {this.state.selection.map(item => (
+            <div className="beam" style={{ height: `${item}rem` }} />
+          ))}
+        </div>
 
-      <button onClick={this.sort}>Sort</button>
-      <button onClick={this.restart}>Restart</button>
-    </StyledSelectionSort> );
+        <button onClick={this.sort}>Sort</button>
+        <button onClick={this.restart}>Restart</button>
+      </StyledSelectionSort>
+    );
   }
 }
- 
+
 export default SelectionSort;
