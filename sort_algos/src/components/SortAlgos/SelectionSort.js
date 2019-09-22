4e9e989885c57143af52afecda8fc930077e361b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-22 18:12:46 +0200

    create class SelectionSort

--- sort_algos/src/components/SortAlgos/SelectionSort.js
+++ sort_algos/src/components/SortAlgos/SelectionSort.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class SelectionSort extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default SelectionSort;
