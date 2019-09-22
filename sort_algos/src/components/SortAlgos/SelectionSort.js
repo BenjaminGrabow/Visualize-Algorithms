46b31c875b51b944197dca6abfa775d5f5cb5b11
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-22 19:33:46 +0200

    create componentDidMount method

--- sort_algos/src/components/SortAlgos/SelectionSort.js
+++ sort_algos/src/components/SortAlgos/SelectionSort.js
@@ -9,9 +9,38 @@ class SelectionSort extends React.Component {
     super(props);
     this.state = { 
       selection: [1],
-      solvedSelection: null
+      selectionSolved: null
      }
   }
+
+  componentDidMount = () => {
+    // create array with random numbers (but unique number)
+
+    for (var a = [], i = 0; i < 40; ++i) a[i] = i;
+
+    function shuffle(array) {
+      var tmp,
+        current,
+        top = array.length;
+      if (top)
+        while (--top) {
+          current = Math.floor(Math.random() * (top + 1));
+          tmp = array[current];
+          array[current] = array[top];
+          array[top] = tmp;
+        }
+      return array;
+    }
+
+    a = shuffle(a);
+    let copy = [...a];
+
+    this.setState({
+      selection: a,
+      selectionSolved: copy.sort((a, b) => a - b)
+    });
+  };
+
   render() { 
     return ( <SelectionSort>
       {this.state.selection.map(item => <div className="beam" style={{height: `${item}rem`}}/>)}
