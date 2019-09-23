744c8124a3b7559f41402dd40878365b42ec3224
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-23 22:33:15 +0200

    create componentDidMount

--- sort_algos/src/components/PathfindingAlgos/AStar.js
+++ sort_algos/src/components/PathfindingAlgos/AStar.js
@@ -7,9 +7,14 @@ class AStar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+
     };
   }
+
+  componentDidMount = () => {
+
+  };
+  
   render() {
     return <StyledAStar></StyledAStar>;
   }
