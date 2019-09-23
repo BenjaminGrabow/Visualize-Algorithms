a5f9899db7ce827d5e5a7e7174a2abc8f5a007b4
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-23 22:18:28 +0200

    create class AStar

--- sort_algos/src/components/PathfindingAlgos/AStar.js
+++ sort_algos/src/components/PathfindingAlgos/AStar.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import styled from "styled-components";
+
+class AStart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default AStart;
