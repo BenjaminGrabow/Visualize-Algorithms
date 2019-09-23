864d072e34e05f7266d82ae8708e126b677afee0
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-23 20:15:10 +0200

    create class quicksort

--- sort_algos/src/components/SortAlgos/RecursiveSorting/QuickSort.js
+++ sort_algos/src/components/SortAlgos/RecursiveSorting/QuickSort.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class QuickSort extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default QuickSort;
