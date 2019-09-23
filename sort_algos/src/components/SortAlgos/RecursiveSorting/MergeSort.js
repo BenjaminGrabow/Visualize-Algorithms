a649e9c94dff9b4816859a5fddeb49c9fb93c488
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-23 20:14:35 +0200

    create class mergesort

--- sort_algos/src/components/SortAlgos/RecursiveSorting/MergeSort.js
+++ sort_algos/src/components/SortAlgos/RecursiveSorting/MergeSort.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class MergeSort extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default MergeSort;
