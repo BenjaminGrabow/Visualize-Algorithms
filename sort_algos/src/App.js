bebe0cb9798cb244c890dd1d499fbfcc4394ba42
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-23 21:31:28 +0200

    delete tabs

--- sort_algos/src/App.js
+++ sort_algos/src/App.js
@@ -3,6 +3,7 @@ import BubbleSort from "./components/SortAlgos/IterativeSorting/BubbleSort";
 import SelectionSort from "./components/SortAlgos/IterativeSorting/SelectionSort";
 import QuickSort from "./components/SortAlgos/RecursiveSorting/QuickSort";
 import MergeSort from "./components/SortAlgos/RecursiveSorting/MergeSort";
+import IterativeTabs from "./components/SortAlgos/IterativeSorting/IterativeTabs";
 import "./App.css";
 import { Route, NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -82,8 +83,9 @@ export default function ScrollableTabsButtonPrevent() {
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        <NavLink to="/bubble_sort">Bubble Sort</NavLink>
-        <NavLink to="/selection_sort">Selection Sort</NavLink>
+        {/* <NavLink to="/bubble_sort">Bubble Sort</NavLink>
+        <NavLink to="/selection_sort">Selection Sort</NavLink> */}
+        <IterativeTabs/>
       </TabPanel>
       <TabPanel value={value} index={1}>
         <NavLink to="/merge_sort">Selection Sort</NavLink>
