d332ca0067775b21f4e31bb99903b1671b973795
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-07 20:29:39 +0200

    created class PathfindingVisualizer

---
+++ sort_algos/src/components/PathfindingAlgos/PathfindingVisualizer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PathfindingVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default PathfindingVisualizer;
